c7ec2aa52def086985ea676be51300da3a742454
Aneesh Saravu Karekad <aneeshsk1996@gmail.com>
2022-05-24 17:11:23 -0400

    chore: Export auth from the config

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { secrets } from "./secrets";
 import "firebase/firestore";
 
@@ -13,3 +14,4 @@ const firebaseConfig = { ...secrets.firebase };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
