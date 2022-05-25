5555e632e4253494ce46d563283a230d637945fe
Aneesh Saravu Karekad <aneeshsk1996@gmail.com>
2022-05-25 13:25:07 -0400

    refractor

--- config/firebase.js
+++ config/firebase.js
@@ -6,9 +6,6 @@ import { secrets } from "./secrets";
 import "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = { ...secrets.firebase };
 // Initialize Firebase
