e9d701b97e73f61574b5df82a06eace433bee636
Aneesh Saravu Karekad <aneeshsk1996@gmail.com>
2022-05-25 01:02:30 -0400

    chore: Export firebase storage

--- config/firebase.js
+++ config/firebase.js
@@ -4,14 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { secrets } from "./secrets";
 import "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = { ...secrets.firebase };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage();
